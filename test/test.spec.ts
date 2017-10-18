9c9c718eb4bdac96ba11d24434a76af45e4b6680
Martin Koptik <martin.koptik@e-invent.eu>
2017-10-18 15:33:16 +0200

    Tests initial commit

---
+++ test/test.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+
+describe("Test", () => {
+    describe("TestOp", () => {
+        it("Should be fine", () => {
+            expect(1).to.equal(1);
+        });
+    })
+});
