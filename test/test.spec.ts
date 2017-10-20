02e648bee43ec8f8f573d162a8b747e9a93e57d8
Martin Koptik <martin.koptik@e-invent.eu>
2017-10-20 10:09:23 +0200

    Removed dummy test

--- test/test.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "chai";
-
-describe("Test", () => {
-    describe("TestOp", () => {
-        it("Should be fine", () => {
-            expect(1).to.equal(1);
-        });
-    })
-});
