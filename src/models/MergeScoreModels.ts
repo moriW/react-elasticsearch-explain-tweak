591e9cb7efe0df29607c0b05e85ebbf2e2610a6b
Martin Koptik <koptik.m@gmail.com>
2017-11-12 10:56:57 +0100

    Merging score components

--- src/models/MergeScoreModels.ts
+++ src/models/MergeScoreModels.ts
@@ -1,24 +1,36 @@
 
-import {ScoreComponent} from "./ScoreComponent";
+import {ScoreComponent, ScoreComponentType} from "./ScoreComponent";
 import {BaseScoreComponent} from "./BaseScoreComponent";
+import {merge} from "d3-array";
 
 export class MergedScoreComponent implements BaseScoreComponent {
 
     label: string;
     id: number;
     result: number;
-    children: BaseScoreComponent[];
+    children: MergedScoreComponent[];
+    type: ScoreComponentType;
 
     constructor(private component1: ScoreComponent, private component2: ScoreComponent) {
         // GET COMPONENT WITH
+        this.type = component1.type;
     }
-
-
-
-
 
 }
 
-export function mergeScoreComponents(components1: ScoreComponent[], components2: ScoreComponent) : MergedScoreComponent[] {
-    return []
+export function mergeScoreComponents(components1: ScoreComponent[], components2: ScoreComponent[]) : MergedScoreComponent[] {
+    const merged: MergedScoreComponent[] = [];
+    components1.forEach(c1 => {
+        const mergeWith = components2.filter(c2 => c1.type == c2.type);
+        const c2 = mergeWith.length > 0 ? mergeWith[0] : null;
+        const mergedComponent = new MergedScoreComponent(c1, c2);
+        mergedComponent.children = mergeScoreComponents(c1.children, c2 != null ? c2.children : []);
+        merged.push(mergedComponent);
+    });
+    components2.filter(c2 => merged.findIndex(m => m.type == c2.type) < 0).forEach(c2 => {
+        const mergedComponent = new MergedScoreComponent(c2, null);
+        mergedComponent.children = mergeScoreComponents(c2.children, []);
+        merged.push(mergedComponent);
+    });
+    return merged
 }
--- test/models/MergedScoreComponent.spec.ts
+++ test/models/MergedScoreComponent.spec.ts
@@ -1,13 +1,13 @@
-
+import {expect} from "chai";
 import {ScoreComponent, ScoreComponentType} from "../../src/models/ScoreComponent";
-import {MergedScoreComponent} from "../../src/models/MergeScoreModels";
+import {MergedScoreComponent, mergeScoreComponents} from "../../src/models/MergeScoreModels";
 
 describe("MergedScoreComponent", () => {
 
     const component1: ScoreComponent = null;
     const component2: ScoreComponent = null;
 
-    const mergedComponent: MergedScoreComponent = new MergedScoreComponent(component1, component2);
+    //const mergedComponent: MergedScoreComponent = new MergedScoreComponent(component1, component2);
 
     it("returns value of first component ordered from highest", () => {
 
@@ -19,23 +19,63 @@ describe("mergeScoreComponent", () => {
 
     const components1 = [
         new ScoreComponent({
+            id: 1,
             type: ScoreComponentType.TfNorm,
             label: "some component",
             result: 1,
             children: [new ScoreComponent({
-
+                id: 2,
+                type: ScoreComponentType.Boost,
+                result: 5,
+                children: []
             })]
         })
     ];
-    const components2 = [];
+    const components2 = [
+        new ScoreComponent({
+            id: 3,
+            type: ScoreComponentType.TfNorm,
+            label: "some component",
+            result: 2,
+            children: [new ScoreComponent({
+                id: 10,
+                type: ScoreComponentType.Boost,
+                result: 5,
+                children: []
+            }), new ScoreComponent({
+                id: 11,
+                type: ScoreComponentType.Idf,
+                result: 6,
+                children: []
+            })]
+        }),
+        new ScoreComponent({
+            id: 4,
+            type: ScoreComponentType.SumOf,
+            label: "some component",
+            result: 1.5,
+            children: []
+        })
+    ];
+
+    const merged = mergeScoreComponents(components1, components2);
 
     it("merges score components by type", () => {
-
+        expect(merged).to.be.lengthOf(2);
+        expect(merged.filter(m => m.type == ScoreComponentType.TfNorm)).to.be.lengthOf(1);
+        expect(merged.filter(m => m.type == ScoreComponentType.SumOf)).to.be.lengthOf(1);
     });
 
-    it("recursively handles a children", () => {
+    it("recursively merges its children", () => {
+        const mergedTfNorm = merged.filter(m => m.type == ScoreComponentType.TfNorm)[0];
+        const mergedSumOf = merged.filter(m => m.type == ScoreComponentType.SumOf)[0];
 
-    });
+        expect(mergedTfNorm.children).to.be.lengthOf(2);
+        expect(mergedSumOf.children).to.be.lengthOf(0);
+
+        expect(mergedTfNorm.children.filter(m => m.type == ScoreComponentType.Boost)).to.be.lengthOf(1);
+        expect(mergedTfNorm.children.filter(m => m.type == ScoreComponentType.Idf)).to.be.lengthOf(1);
+    })
 
 
 
